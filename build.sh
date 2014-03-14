0b021d9a5b0a8d7b668410fc8d6037313a537815
rfraposa <rich@hortonworks.com>
2014-03-14 13:40:12 -0600

    Added *** to build.sh output

--- build.sh
+++ build.sh
@@ -6,40 +6,39 @@
 cd ./dockerfiles/ambari_node_base
 x=$(docker images | grep -c  hwx/ambari_node_base)
 if [ x -eq 0 ]; then
-	echo "\n*** Building hwx/ambari_node_base image... ***\n"
+	echo -e "\n*** Building hwx/ambari_node_base image... ***\n"
 	docker build -t hwx/ambari_node_base .
-	echo "Build of hwx/ambari_node_base complete!"
+	echo -e "\n*** Build of hwx/ambari_node_base complete! ***\n"
 else
-	echo "\n*** hwx/ambari_node_base image already built ***\n"
+	echo -e "\n*** hwx/ambari_node_base image already built ***\n"
 fi
 
 # Build hwx/ambari_node
-echo "\n*** Building hwx/ambari_node ***\n"
+echo -e "\n*** Building hwx/ambari_node ***\n"
 cd ../ambari_node
 docker build -t hwx/ambari_node .
-echo "\n*** Build of hwx/ambari_node complete! ***\n"
+echo -e "\n*** Build of hwx/ambari_node complete! ***\n"
 
 # Build hwx/ambari_server_base
 # This image is probably pre-built on the VM
 y=$(docker images | grep -c  hwx/ambari_server_base)
 if [ y -eq 0 ]; then
-	echo "\n*** Building hwx/ambari_server_base. This may take a long time!!! ***\n"
+	echo -e "\n*** Building hwx/ambari_server_base. This may take a long time!!! ***\n"
 	cd ../ambari_server_base
 	docker build -t hwx/ambari_server_base .
-	echo "\n*** Build of hwx/ambari_server_base complete! ***\n"
+	echo -e "\n*** Build of hwx/ambari_server_base complete! ***\n"
 else 
-	echo "\n*** hwx/ambari_server_base image already built ***\n"
+	echo -e "\n*** hwx/ambari_server_base image already built ***\n"
 fi
 
 # Build hwx/ambari_server
-echo "\n*** Building hwx/ambari_server ***\n"
+echo -e "\n*** Building hwx/ambari_server ***\n"
 cd ../ambari_server
 docker build -t hwx/ambari_server .
-echo "\n*** Build of hwx/ambari_server complete! ***\n"
+echo -e "\n*** Build of hwx/ambari_server complete! ***\n"
 
 # Copy utility scripts into /root/scripts, which is already in the PATH
 echo "Copying utility scripts..."
 cp ../../scripts/* /root/scripts/
 
-echo ""
-echo "\n*** The images have successfully been built for this classroom VM ***\n"
+echo -e "\n*** The images have successfully been built for this classroom VM ***\n"
