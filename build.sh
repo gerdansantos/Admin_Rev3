bdf2ea5149783a2bb61679a596bdf07da4e7e1be
rfraposa <rich@hortonworks.com>
2014-03-14 13:17:35 -0600

    build.sh now checks for existing docker images

--- build.sh
+++ build.sh
@@ -3,10 +3,15 @@
 # Build the Docker images
 
 # Build hwx/ambari_node_base first
-echo "Building hwx/ambari_node_base image..."
 cd ./dockerfiles/ambari_node_base
-docker build -t hwx/ambari_node_base .
-echo "Build of hwx/ambari_node_base complete!"
+x=$(docker images | grep -c  hwx/ambari_node_base)
+if [ x -eq 0 ]; then
+	echo "Building hwx/ambari_node_base image..."
+	docker build -t hwx/ambari_node_base .
+	echo "Build of hwx/ambari_node_base complete!"
+else
+	echo "hwx/ambari_node_base image already built"
+fi
 
 # Build hwx/ambari_node
 echo "Building hwx/ambari_node"
@@ -15,11 +20,16 @@ docker build -t hwx/ambari_node .
 echo "Build of hwx/ambari_node complete!"
 
 # Build hwx/ambari_server_base
-# This image is pre-built on the VM
-# echo "Building hwx/ambari_server_base. This may take a long time!!!"
-# cd ../ambari_server_base
-# docker build -t hwx/ambari_server_base .
-# echo "Build of hwx/ambari_server_base complete!"
+# This image is probably pre-built on the VM
+y=$(docker images | grep -c  hwx/ambari_server_base)
+if [ y -eq 0 ]; then
+	echo "Building hwx/ambari_server_base. This may take a long time!!!"
+	cd ../ambari_server_base
+	docker build -t hwx/ambari_server_base .
+	echo "Build of hwx/ambari_server_base complete!"
+else 
+	echo "hwx/ambari_server_base image already built"
+fi
 
 # Build hwx/ambari_server
 echo "Building hwx/ambari_server"
