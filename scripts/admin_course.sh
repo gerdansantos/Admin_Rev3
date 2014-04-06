6b5cb971b944ae81f86ebc357fc4f7d24fde721a
Michael Bingham <michael.b.bingham@gmail.com>
2014-04-06 16:40:55 -0700

    Updated container launch scripts to open more ports

--- scripts/admin_course.sh
+++ scripts/admin_course.sh
@@ -2,11 +2,15 @@
 
 #Start node1, which will be running the ambari-server process
 echo "Starting node1..."
-CID_namenode=$(docker run -d --privileged=true --dns 127.0.0.1 -p 8080:8080 -p 8440:8440 -p 8441:8441 -p 50070:50070 -p 8020:8020 -e NODE_TYPE=namenode --name node1 -h node1 -i -t hwx/ambari_server)
+CID_namenode=$(docker run -d --privileged=true --dns 127.0.0.1 -p 8080:8080 -p 8440:8440 -p 8441:8441 -p 50070:50070 -p 50075:50075 -p 8020:8020 -e NODE_TYPE=namenode --name node1 -h node1 -i -t hwx/ambari_server)
 IP_namenode=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" node1)
 echo "node1 started at $IP_namenode"
 
-for (( i=2; i<=4; ++i));
+CID=$(docker run -d --dns 127.0.0.1 --privileged=true -h node2 --name node2 -p 22 --link node2:node2 -p 8440 -p 8441 -p 8088:8088 -p 19888:19888 -i -t hwx/ambari_node)
+IP_node2=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" node2)
+echo "node1 started at $IP_node2"
+
+for (( i=3; i<=4; ++i));
 do
 nodename="node$i"
 CID=$(docker run -d --dns 127.0.0.1 --privileged=true -h $nodename --name $nodename -p 22 --link node1:node1 -p 8440 -p 8441 -i -t hwx/ambari_node)
