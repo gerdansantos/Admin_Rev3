5a2b2b3968ece03e0d86d51408d07d7342608667
Michael Bingham <michael.b.bingham@gmail.com>
2014-04-06 17:58:23 -0700

    Moved some lab files around so that both ambari server and regular nodes have the same files

--- dockerfiles/ambari_node/scripts/startup.sh
+++ dockerfiles/ambari_node/scripts/startup.sh
@@ -6,13 +6,16 @@ chkconfig ntpd on
 /etc/init.d/sshd start
 /etc/init.d/ntpd start
 
-# start DNS
+setenforce 0
+chkconfig iptables off
+/etc/init.d/iptables stop
+
+# Setup the hosts file and start DNS
 service dnsmasq restart
+
+export datanode_ip=$(ip addr | grep inet | grep eth0 | awk -F" " '{print $2}' | sed -e 's/\/.*$//')
 
 grep -v rootfs /proc/mounts > /etc/mtab
 umask 022
-
-service httpd start
-ambari-server start
 
 bash
